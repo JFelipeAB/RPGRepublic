52e1bda78b397a18ba472e1e608522083c957ce4
jfelipeAb <joao-alves-borges@hotmail.com>
2022-04-19 18:34:43 -0300

    ficha

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 //mongoose.connect(process.env.URLDataBaseConcetion);
 
-//mongoose.connect('mongodb+srv://dev:aFj3UZRYSGifbeub@cluster0.mzipn.mongodb.net/RPG_Republic_PRD?authSource=admin&replicaSet=atlas-9nw973-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true');
+mongoose.connect('mongodb+srv://dev:aFj3UZRYSGifbeub@cluster0.mzipn.mongodb.net/RPG_Republic_PRD?authSource=admin&replicaSet=atlas-9nw973-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true');
 
 //mongoose.connect('mongodb://localhost:27017/RPG_Republic_HMG_local')
 
