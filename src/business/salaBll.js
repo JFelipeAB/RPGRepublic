629b4e0425c193d735d6e4f29dc828494a7d5720
João Felipe <57374143+JFelipeAB@users.noreply.github.com>
2022-04-20 22:34:47 -0300

    ajuste

--- src/business/salaBll.js
+++ src/business/salaBll.js
@@ -5,10 +5,18 @@ var sala = function () {
         let listaSala = [{
             idSala: 5,
             descricao: "Sala1",
-            usuario: {
-                idUsuario: "1",
-                nome: "Usuario123"
+            usuarioMestre: {
+                login: "Adm",
+                icon: "../aditional/Icon/IconeCaveira.png"
             },
+            listaUsuario: [{
+                login: "Clebinho123",
+                icon: "../aditional/Icon/IconeEspadaFogo.jpg"
+            },
+            {
+                login: "20Derrotar",
+                icon: "../aditional/Icon/IconeEspadaFogo.jpg"
+            }],
             senha: "123"
         }
         ];
@@ -16,12 +24,12 @@ var sala = function () {
         return listaSala;
     };
 
-    var getSala = function (idSalaPesquisa) {        
+    var getSala = function (idSalaPesquisa) {
         let sala = listaSala().find(sala => sala.idSala == idSalaPesquisa);
         return sala;
     };
 
-    var salvarSala = function (){
+    var salvarSala = function () {
 
     };
 
