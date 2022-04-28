a3a488579a4e654153b3ce5453a20a231e0cca5f
Andressa Alves Boorges <alvesaborges@gmail.com>
2022-04-28 10:47:55 -0300

    telas

--- views/content/adicional/perfil.js
+++ views/content/adicional/perfil.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 });
 
 var perfil = function () {
-    
+
     var configFIcha = function () {
         debugger;
         $('#imgIcone').attr("src", usuario.icon);
@@ -11,8 +11,27 @@ var perfil = function () {
         $('#lblNivel').text("Nível " + usuario.nivel + " XP: ");
         $('#lblEmail').text("E-mail: " + usuario.eMail);
         $('#lblRank').text("Rank: " + usuario.acesso);
-        // $('#').val("");
-        // $('#').val("");
+        let contador = 0;
+        usuario.listaIten.forEach(iten => {
+
+            switch (iten.tipo) {
+                case 'icone':
+                    $('#divIcones').append(' <img id="Icone'+contador +'" src="'+ iten.descricao +'" width="200px">');
+                    break;
+                case 'FontFamily':
+                    $('#divFontes').append('<div id="Icone'+contador+'">Fonte: '+ iten.descricao+'</div>');
+                    $('#Icone'+contador).css("font-family", iten.descricao );
+                    break;
+                case 'FontColor':
+                    $('#divCorTexto').append('Cor de texto: '+iten.descricao+' <input disabled id="Icone'+contador+'" type="color" value="'+iten.cor+'">');
+                    break;
+                    case 'moeda':
+                        break;
+                default:
+                    alert(`Erro, contate o administrador! tipo de item não cadastrado: ${iten.tipo}.`);
+            };
+            contador++;
+        });
     };
 
     return {
