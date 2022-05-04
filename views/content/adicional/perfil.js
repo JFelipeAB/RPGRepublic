9f6c247639097ea5a5429d1c9a029366c14eaf0f
João Felipe <57374143+JFelipeAB@users.noreply.github.com>
2022-05-03 23:34:10 -0300

    ajuste perfil

--- views/content/adicional/perfil.js
+++ views/content/adicional/perfil.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
 var perfil = function () {
 
     var configFIcha = function () {
-        debugger;
         $('#imgIcone').attr("src", usuario.icon);
         $('#lblLogin').text(usuario.login);
         $('#lblNivel').text("Nível " + usuario.nivel + " XP: ");
@@ -13,23 +12,23 @@ var perfil = function () {
         $('#lblRank').text("Rank: " + usuario.acesso);
         let contador = 0;
         usuario.listaIten.forEach(iten => {
-
+            debugger;
             switch (iten.tipo) {
                 case 'icone':
-                    $('#divIcones').append(' <img id="Icone'+contador +'" src="'+ iten.descricao +'" width="200px">');
-                    $("#divIcones").on("click", perfil.confirmarSelecionarItem(iten.tipo, "Icone" + contador));
+                    $('#divIcones').append(' <img id="Icone' + contador + '" src="' + iten.descricao + '" width="200px">');
+                    $("#divIcones").on("click", function () { perfil.selecionarItem(iten.tipo, contador) });
                     break;
                 case 'FontFamily':
-                    $('#divFontes').append('<div id="Icone'+contador+'">Fonte: '+ iten.descricao+'</div>');
-                    $('#Icone'+contador).css("font-family", iten.descricao );
-                    $("#divFontes").on("click", perfil.confirmarSelecionarItem(iten.tipo, "Icone" + contador));
+                    $('#divFontes').append('<div id="Icone' + contador + '">Fonte: ' + iten.descricao + '</div>');
+                    $('#Icone' + contador).css("font-family", iten.descricao);
+                    $("#divFontes").bind("click", function () { perfil.selecionarItem(iten.tipo, contador) });
                     break;
                 case 'FontColor':
-                    $('#divCorTexto').append('Cor de texto: '+iten.descricao+' <input disabled id="Icone'+contador+'" type="color" value="'+iten.cor+'">');
-                    $("#divCorTexto").on("click", perfil.confirmarSelecionarItem(iten.tipo, "Icone" + contador));
+                    $('#divCorTexto').append('Cor de texto: ' + iten.descricao + ' <input disabled id="Icone' + contador + '" type="color" value="' + iten.cor + '">');
+                    $("#divCorTexto").bind("click", function () { perfil.selecionarItem(iten.tipo, contador) });
                     break;
-                    case 'moeda':
-                        break;
+                case 'moeda':
+                    break;
                 default:
                     alert(`Erro, contate o administrador! tipo de item não cadastrado: ${iten.tipo}.`);
             };
@@ -37,23 +36,16 @@ var perfil = function () {
         });
     };
 
-    var confirmarSelecionarItem = function(tipo, index){
-        if(!tipo || !index)
-        {
+    var selecionarItem = function (tipo, index) {
+        debugger;
+        if (!tipo) {
             alert(`Erro, contate o administrador! tipo de item não encontrado!`);
             return;
         }
-        var modal = new bootstrap.Modal($('#mdlConfirmarItem'), {
-            keyboard: false
-        });
-        $("#btnModalConfirmar").on("click",perfil.selecionarItem(tipo, index));
-        modal.show('slow'); 
-    };
-
-    var selecionarItem = function(tipo, index){
         switch (tipo) {
             case 'icone':
-               console.log('mudar icone');
+                usuario.icone = usuario.listaIten[index].descricao
+                $('#imgIcone').attr("src", usuario.icone);
                 break;
             case 'FontFamily':
                 console.log('mudar icone');
@@ -61,8 +53,8 @@ var perfil = function () {
             case 'FontColor':
                 console.log('mudar icone');
                 break;
-                case 'moeda':
-                    break;
+            case 'moeda':
+                break;
             default:
                 alert(`Erro, contate o administrador! tipo de item não cadastrado: ${iten.tipo}.`);
         };
@@ -70,7 +62,6 @@ var perfil = function () {
 
     return {
         configFIcha: configFIcha,
-        confirmarSelecionarItem: confirmarSelecionarItem,
         selecionarItem: selecionarItem
     };
 
