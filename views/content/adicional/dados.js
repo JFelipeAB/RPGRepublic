20e259b247d5aabc783d0bb4c1e2aae77b405b9c
João Felipe <57374143+JFelipeAB@users.noreply.github.com>
2022-06-02 00:26:59 -0300

    ajuste

--- views/content/adicional/dados.js
+++ views/content/adicional/dados.js
@@ -23,8 +23,7 @@ var sala = function () {
         });        
     };
     
-    var adicionarMensagem = function (text) {
-        debugger;
+    var adicionarMensagem = function (text) {       
         if (text)
             socket.emit("connection", `<div style="font-family:${usuario.textoFonte}; color:${usuario.textoCorN}"` +
                 `class="textoEditavel">${usurImg}<strong>${usurName}</strong>: ${text}</div><hr>`);
